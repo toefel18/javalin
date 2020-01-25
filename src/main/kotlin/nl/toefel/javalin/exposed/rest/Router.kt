6180e3c2466a7451639b112e3d36c7980aa342a3
Christophe Hesters <toefel18@gmail.com>
2020-01-25 21:51:58 +0100

    Working CF example

--- src/main/kotlin/nl/toefel/javalin/exposed/rest/Router.kt
+++ src/main/kotlin/nl/toefel/javalin/exposed/rest/Router.kt
@@ -1,5 +1,6 @@
 package nl.toefel.javalin.exposed.rest
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.javalin.Javalin
 import io.javalin.http.Context
 import nl.toefel.javalin.exposed.dto.ActorDto
@@ -7,14 +8,16 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
+import java.util.function.Supplier
 
 class Router(val port: Int) {
     private val logger: Logger = LoggerFactory.getLogger(Router::class.java)
-    private val executor = Executors.newFixedThreadPool(64)
+    private val executor = Executors.newCachedThreadPool()
+    private val mapper = ObjectMapper().findAndRegisterModules()
 
     val app = Javalin.create { cfg -> cfg.requestLogger(::logRequest).enableCorsForAllOrigins() }
-        .get("/actors", ::getActor)
-        .get("/actors-future", ::getActorFuture)
+        .get("/perf/instant", ::getActor)
+        .get("/perf/future", ::getActorFuture)
 
     private fun logRequest(ctx: Context, executionTimeMs: Float) =
         logger.info("${ctx.method()} ${ctx.fullUrl()} status=${ctx.status()} durationMs=$executionTimeMs")
@@ -25,7 +28,6 @@ class Router(val port: Int) {
     }
 
     fun getActor(ctx: Context) {
-        logger.info("GET " + ctx.req.requestURL)
         val delay = ctx.queryParam("delayMs")?.toLong() ?: 0
         if (delay > 0) {
             Thread.sleep(delay)
@@ -38,18 +40,18 @@ class Router(val port: Int) {
     }
 
     fun getActorFuture(ctx: Context) {
-        logger.info("GET " + ctx.req.requestURL)
         val delay = ctx.queryParam("delayMs")?.toLong() ?: 0L
-        val cf = CompletableFuture.supplyAsync<ActorDto>({
+        val cf = CompletableFuture.supplyAsync(Supplier<String> {
             if (delay > 0) {
                 Thread.sleep(delay)
             }
-            ActorDto(
-                id = 1,
-                firstName = "TestDto",
-                lastName = "Jemeur"
-            )
-        }, executor::execute)
+            mapper.writeValueAsString(
+                ActorDto(
+                    id = 1,
+                    firstName = "TestDto",
+                    lastName = "Jemeur"
+                ))
+        }, executor)
         ctx.result(cf)
     }
 }
