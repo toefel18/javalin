1fc070708677f161af808764a3187ac4991d2cc5
Christophe Hesters <toefel18@gmail.com>
2020-01-25 23:30:19 +0100

    Simplify

--- src/main/kotlin/nl/toefel/javalin/exposed/dto/ActorDto.kt
+++
@@ -1,7 +0,0 @@
-package nl.toefel.javalin.exposed.dto
-
-data class ActorDto(
-    val id: Int?,
-    val firstName: String,
-    val lastName: String
-)
--- src/main/kotlin/nl/toefel/javalin/performance/Main.kt
+++ src/main/kotlin/nl/toefel/javalin/performance/Main.kt
@@ -1,23 +1,32 @@
-package nl.toefel.javalin.exposed.rest
+package nl.toefel.javalin.performance
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.javalin.Javalin
 import io.javalin.http.Context
-import nl.toefel.javalin.exposed.dto.ActorDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.function.Supplier
 
-class Router(val port: Int) {
+fun main() {
+    Router(8080).start()
+}
+
+data class CustomerDto(
+    val id: Int?,
+    val firstName: String,
+    val lastName: String
+)
+
+class Router(private val port: Int) {
     private val logger: Logger = LoggerFactory.getLogger(Router::class.java)
     private val executor = Executors.newCachedThreadPool()
     private val mapper = ObjectMapper().findAndRegisterModules()
 
     val app = Javalin.create { cfg -> cfg.requestLogger(::logRequest).enableCorsForAllOrigins() }
-        .get("/perf/instant", ::getActor)
-        .get("/perf/future", ::getActorFuture)
+        .get("/perf/instant", ::getCustomer)
+        .get("/perf/future", ::getCustomerFuture)
 
     private fun logRequest(ctx: Context, executionTimeMs: Float) =
         logger.info("${ctx.method()} ${ctx.fullUrl()} status=${ctx.status()} durationMs=$executionTimeMs")
@@ -27,26 +36,26 @@ class Router(val port: Int) {
         return this
     }
 
-    fun getActor(ctx: Context) {
+    private fun getCustomer(ctx: Context) {
         val delay = ctx.queryParam("delayMs")?.toLong() ?: 0
         if (delay > 0) {
             Thread.sleep(delay)
         }
-        ctx.json(ActorDto(
+        ctx.json(CustomerDto(
             id = 1,
             firstName = "TestDto",
             lastName = "Jemeur"
         ))
     }
 
-    fun getActorFuture(ctx: Context) {
+    private fun getCustomerFuture(ctx: Context) {
         val delay = ctx.queryParam("delayMs")?.toLong() ?: 0L
         val cf = CompletableFuture.supplyAsync(Supplier<String> {
             if (delay > 0) {
                 Thread.sleep(delay)
             }
             mapper.writeValueAsString(
-                ActorDto(
+                CustomerDto(
                     id = 1,
                     firstName = "TestDto",
                     lastName = "Jemeur"
@@ -55,3 +64,4 @@ class Router(val port: Int) {
         ctx.result(cf)
     }
 }
+
